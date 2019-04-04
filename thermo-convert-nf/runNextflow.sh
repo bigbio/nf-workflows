bdb35f1f28c8dc64022888073001184acb74e255
Suresh Hewapathirana <hewapathirana@ebi.ac.uk>
2019-04-04 17:12:46 +0100

    rename variables

--- thermo-convert-nf/runNextflow.sh
+++ thermo-convert-nf/runNextflow.sh
@@ -3,11 +3,12 @@
 ACCESSION=$1
 USERNAME=$2
 PASSWORD=$3
-NEXTFLOW_DIR=$4
-${NEXTFLOW_DIR}nextflow run thermo-convert-nf.nf -name $ACCESSION \
+METADATA_DIR=$4
+
+${NEXTFLOW}nextflow run thermo-convert-nf.nf -name $ACCESSION \
                                   -c nextflow.config \
                                   --px_accession $ACCESSION \
                                   --pride_username $USERNAME \
                                   --pride_password $PASSWORD \
-                                  --metadata_path $NEXTFLOW_DIR + "data/" + $ACCESSION
-${NEXTFLOW_DIR}nextflow clean -f $ACCESSION
+                                  --metadata_path $METADATA_DIR + "/data/" + $ACCESSION
+${NEXTFLOW}nextflow clean -f $ACCESSION
